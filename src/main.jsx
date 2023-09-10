afa586146b203c8fa875391d1c947d91ccd96cf9
OnlyCrays <cristhian.amaya13@gmail.com>
2023-09-10 17:46:10 -0400

    add auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import './index.css'
 
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Logout from './pages/Logout'
+import NotFound from './pages/NotFound'
+
+import ProtectedRoute from './layouts/ProtectedRoute'
+import { AuthProvider } from './auth/AuthProvider'
+
+const router = createHashRouter([
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/logout',
+		element: <Logout />,
+	},
+	{
+		path: '/',
+		element: <ProtectedRoute />,
+		children: [
+			{
+				path: '/dashboard',
+				element: <Dashboard />,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <NotFound />,
+	},
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<AuthProvider>
+		<RouterProvider router={router} />
+	</AuthProvider>,
 )
